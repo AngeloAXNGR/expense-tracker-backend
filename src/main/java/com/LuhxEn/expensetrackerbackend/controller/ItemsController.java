3282acc7a195e801a595502ac31e9c0c5e81aead
Angelo Santos <axngrsantos@gmail.com>
2023-02-11 15:28:06 +0800

    Add function for reading all item data and adding item data

--- src/main/java/com/LuhxEn/expensetrackerbackend/controller/ItemsController.java
+++ src/main/java/com/LuhxEn/expensetrackerbackend/controller/ItemsController.java
@@ -1,4 +1,41 @@
 package com.LuhxEn.expensetrackerbackend.controller;
 
+import com.LuhxEn.expensetrackerbackend.model.Items;
+import com.LuhxEn.expensetrackerbackend.repository.ExpenseRepository;
+import com.LuhxEn.expensetrackerbackend.repository.ItemsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
 public class ItemsController {
+    @Autowired
+    ExpenseRepository expenseRepository;
+
+    @Autowired
+    ItemsRepository itemsRepository;
+
+    @GetMapping("/expenses/{expenseId}/items")
+    public ResponseEntity<List<Items>> getAllItemsByExpenseId(@PathVariable(value="expenseId") long expenseId){
+        if(!expenseRepository.existsById(expenseId)){
+            throw new RuntimeException();
+        }
+
+        List<Items> items = itemsRepository.findByExpenseId(expenseId);
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+
+    @PostMapping("/expenses/{expenseId}/items")
+    public ResponseEntity<Items> createItems(@PathVariable(value="expenseId") long expenseId, @RequestBody Items itemRequest){
+        Items item = expenseRepository.findById(expenseId).map(expense -> {
+            itemRequest.setExpense(expense); // Associating created item to a specific expense FK which is expenseId
+            return itemsRepository.save(itemRequest);
+        }).orElseThrow(() -> new RuntimeException());
+        return new ResponseEntity<>(item, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/LuhxEn/expensetrackerbackend/repository/ItemsRepository.java
+++ src/main/java/com/LuhxEn/expensetrackerbackend/repository/ItemsRepository.java
@@ -3,5 +3,8 @@ package com.LuhxEn.expensetrackerbackend.repository;
 import com.LuhxEn.expensetrackerbackend.model.Items;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemsRepository extends JpaRepository<Items, Long> {
+    List<Items> findByExpenseId(long expenseId);
 }
